081720383479e8b2cbf92365383b8460f8df44ce
keniossai <kenossai1@gmail.com>
2023-05-03 21:09:52 +0100

    Update on app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -190,14 +190,6 @@ Author URI: https://themeforest.net/user/ashishmaraviya
     </div>
     <!-- Whatsapp end -->
 
-    <!-- Back to top -->
-    <div class="progress-wrap">
-        <i class="fa fa-arrow-up" aria-hidden="true"></i>
-        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
-            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
-        </svg>
-    </div>
-    <!-- Back to top -->
 
     <!-- Vendor JS -->
     <script src="{{ asset('assets') }}/js/vendor/jquery-3.1.1.min.js"></script>
